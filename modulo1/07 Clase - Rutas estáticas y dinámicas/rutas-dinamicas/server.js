6be840d82e864f0f8f4a272401b4e59e9583ac33
MelinaFigueroa <138631344+MelinaFigueroa@users.noreply.github.com>
2023-08-02 15:19:08 -0300

    se agregan rutas id/nombre

--- modulo1/07 Clase - Rutas estáticas y dinámicas/rutas-dinamicas/server.js
+++ modulo1/07 Clase - Rutas estáticas y dinámicas/rutas-dinamicas/server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const cursos = require('./cursos');
 const app = express();
 const dotenv = require('dotenv');
@@ -6,76 +6,74 @@ const dotenv = require('dotenv');
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
+// routes
 app.get('/', (req, res) => {
-    res.send('Bienvenid@s al servidor web con rutas dinámicas!'); 
+    res.send('Bienvenidos al servidor web de rutas dinámicas');
 });
-
+app.get('/curso', (req, res) => {
+    const queryParams = Object.keys(req.query)
+    if (queryParams.length === 0) {
+        res.json(cursos);
+    } else {
+        let resultado = [];
+        for (let curso of cursos) {
+            console.log(req.query);
+            if (curso.nombre.toLowerCase() === String(req.query.nombre).toLowerCase()
+                && curso.categoria.toLowerCase() === String(req.query.categoria).toLowerCase()) {
+                resultado.push(curso)
+            }
+        }
+        resultado.length > 0 ?
+            res.json(resultado) : res.json({ id: 'ERROR', description: req.query.nombre })
+    }
+});
 // url params
 app.get('/cursos/:categoria', (req, res) => {
-  let parametro = req.params.categoria.trim().toLowerCase();
-  console.log(parametro)
-  if (parametro !== '') {
-    let resultado = []
-    for (let curso of cursos) {
-      if (curso.categoria.toLowerCase() === parametro) {
-        resultado.push(curso)
-      }
+    let parametro = req.params.categoria.trim().toLowerCase();
+    console.log(parametro)
+    if (parametro !== '') {
+        let resultado = []
+        for (let curso of cursos) {
+            if (curso.categoria.toLowerCase() === parametro) {
+                resultado.push(curso)
+            }
+        }
+        resultado.length > 0 ?
+            res.json(resultado) : res.json({ id: 'ERROR', description: 'No se encuentra coincidencias' })
     }
-    // opcion 1 de resultado --> if/else comun
-    // if (resultado.length > 0) {
-    //   res.json(resultado) 
-    // } else {
-    //   res.json({id:'ERROR', description:'No se encuentra coincidencias'})
-    // }
-
-    // opcion 2 ---> triada --> 
-    // condicion ? resultado si cumple la condicion: resultado si no cumple la condicion
-    resultado.length > 0 ? 
-      res.json(resultado) : res.json({id:'ERROR', description:'No se encuentra coincidencias'})
-  }
- })
-
-app.get('/cursos', (req, res) => { 
-  const queryParams = Object.keys(req.query)
-  if (queryParams.length === 0) {
-    res.json(cursos);
-  } else {
-    let resultado = [];
-    for (let curso of cursos) {
-      res.send( typeof req.query.id)
-      // && es and 
-      // valor1 | valor2 | result
-      // true | true | true |
-      // false | true | false
-      // true false false
-      // false | false | true
-      // || es or
-      // valor1 | valor2 | result
-      // true | true | true 
-      // false | true | true
-      // true | false |true
-      // false | false | false
-      console.log(req.query)
-      if (curso.nombre.toLowerCase() === String(req.query.nombre).toLowerCase()
-        && curso.categoria.toLowerCase() === String(req.query.categoria).toLowerCase()) {
-                    resultado.push(curso)
-                }
+});
+app.get('/cursos/codigo/:id', (req, res) => {
+    let id = req.params.id;
+    console.log(id);
+    if (id !== '') {
+        let resultado = []
+        for (let curso of cursos) {
+            if (curso.id === Number(id)) {
+                resultado.push(curso)
+            }
+        }
+        resultado.length > 0 ?
+            res.json(resultado) : res.json({ id: 'ERROR', description: 'No se encuentra coincidencias' })
     }
-    resultado.length > 0 ? 
-      res.json(resultado) : res.json({id:'ERROR', description:req.query.nombre})
-  }
-})
+});
+app.get('/curso/nombre/:nombre', (req, res) => {
+    let nombre = req.params.nombre.trim().toLowerCase();
+    console.log(nombre);
+    if (nombre !== '') {
+        let resultado = []
+        for (let curso of cursos) {
+            if (curso.nombre.toLowerCase() === nombre) {
+                resultado.push(curso)
+            }
+        }
+        resultado.length > 0 ?
+            res.json(resultado) : res.json({ id: 'ERROR', description: 'No se encuentra coincidencias' })
+    }
+});
+app.get('*', (req, res) => {
+    res.status(404).send('Lo siento, la página que buscas no existe');
+});
 
-app.get('/curso/codigo/:id', (req, res) => { })
-
-app.get('/curso/nombre/:nombre', (req, res) => { })
-
-// rutas inexistentes
-app.get("*", (req, res) => { 
-    res.status(404).send("Lo siento, pero esta pagina que buscas no existe."); 
-}); 
-
-// Inicia el servidor
-  app.listen(PORT, () => {
-    console.log(`Servidor iniciado en el puerto ${PORT}`);
-  });
+app.listen(PORT, () => {
+    console.log(`API is listening on port ${PORT}`);
+});
